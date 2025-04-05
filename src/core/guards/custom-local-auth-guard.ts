a88b8c655810ac5506b4786f5d606db0c76ba5a9
Mikhail Sergeev <sergeevmikhail.dev@gmail.com>
2025-04-05 23:41:21 +0400

    change guards comment

--- src/core/guards/custom-local-auth-guard.ts
+++ src/core/guards/custom-local-auth-guard.ts
@@ -4,7 +4,7 @@ import { AuthGuard } from '@nestjs/passport';
 @Injectable()
 export class CustomLocalAuthGuard extends AuthGuard('local') {
   handleRequest(err: any, user: any) {
-    // Не бросаем ошибку сразу, а возвращаем null
+    //не бросаем ошибку сразу, а возвращаем null
     if (err || !user) {
       return null; // ThrottlerGuard сможет посчитать запрос
     }
